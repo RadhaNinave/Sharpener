41b1be7d42ccd301ec2a0e434b24b87f0cab867b
RadhaNinave <radhikarao245@gmail.com>
2022-11-27 13:47:50 +0530

    first commit

---
+++ dom.js
@@ -0,0 +1,11 @@
+//console.log(document.domain);
+console.dir(document);
+document.title=123;
+console.log(document.all);
+console.log(document.getElementById('header-title'));
+var headerTitle=document.getElementById('header-title');
+headerTitle.style.fontWeight='bold';
+//headerTitle.style.color='green';
+var titles=document.getElementById('title');
+titles.style.color='green';
+titles.style.fontWeight='bold';
