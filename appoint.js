d41bc6015d83e0241d143557782f635183cc3a5f
RadhaNinave <101126042+RadhaNinave@users.noreply.github.com>
2022-12-08 15:07:03 +0530

    Add files via upload

--- appoint.js
+++ appoint.js
@@ -1,41 +1,85 @@
-var form=document.getElementById('add-form');
-//console.log(name);
-form.addEventListener('submit',addItem );
+const form = document.querySelector('#addForm');
+let itemList = document.querySelector('#items');
+form.addEventListener('submit',saveToLocal);
 
-function addItem(e)
-{
+function saveToLocal(e){
     e.preventDefault();
-    var name=document.getElementById('name').value;
-    var email=document.getElementById('mail').value;
-    console.log(name);
-    console.log(email);
-    var multiUser=[];
-    var user = {
-        name: name,
-        email: email
-        
-      };
-      multiUser.push(user);
-      multiUser.push(JSON.parse(localStorage.getItem('multiUser')));
-      console.log(multiUser);
-    
-    
-      localStorage.setItem(user.email, JSON.stringify(multiUser));
-      Object.keys(localStorage).forEach((key) => {
-
-
-
-        stringifiedDetailsOfPeople = localStorage.getItem(user.email);
-        
-        detailsOfPeople = JSON.parse(stringifiedDetailsOfPeople);
-        
-        
-        
-        addNewLineElement(detailsOfPeople);
-        
-        });
-
-
-      
-
+    //const newItem = document.querySelector('#item').value;
+    const userName = document.querySelector('#user').value;
+    const email = document.querySelector('#email').value;
+    const obj = {
+       // newItem,
+       userName,
+        email
+    }
+   // localStorage.setItem(obj.email,JSON.stringify(obj));
+   axios.post("https://crudcrud.com/api/db6816ee0baa49d2b4217acf4c1c6f84/appointmentData",obj)
+   .then(resolve =>{
+    console.log(resolve)
+   })
+   .catch(err =>
+    {
+      console.log(err)
+    })
+    showNewUserOnScreen(obj);
 }
+
+/*window.addEventListener('DOMContentLoaded',() => {
+    const localStorageObj = localStorage;
+    const localStorageKeys = Object.keys(localStorageObj);
+    for(let i=0;i<localStorageKeys.length;i++){
+        const key = localStorageKeys[i];
+        const userDetailsString = localStorageObj[key];
+        const userDetailsObj = JSON.parse(userDetailsString);
+        showNewUserOnScreen(userDetailsObj);
+    }
+});*/
+
+function showNewUserOnScreen(user){
+    //document.querySelector('#item').value = '';
+    document.querySelector('#user').value = '';
+    document.querySelector('#email').value = '';
+    if(localStorage.getItem(user.email) !== null){
+        removeUserFromScreen(user.email);
+    }
+    const ul = document.querySelector('#items');
+    const childHTTML = `<li class="list-group-item" id=${user.email}> <br>User: ${user.userName} email id :${user.email}
+                        <span id=${user.userName} hidden>${user.use}</span>
+                        <span id=${user.email} hidden>${user.email}</span>
+                        <button class="btn btn-danger btn-sm float-right delete" onclick=deleteUser('${user.email}')>Delete</button>                        
+                        <button class="btn btn-danger btn-sm float-right delete" style="background-color:#f4f4f4; color:black" onclick=editUserDetails('${user.email}','${user.userName}')>Edit</button>
+                        </li>`;
+    // console.log(user.email);
+    // console.log(user.userName);
+    // console.log(user.newItem);
+    ul.innerHTML = ul.innerHTML+childHTTML;
+}
+
+function removeUserFromScreen(emailId){
+    const parentNode = document.querySelector('#items');
+    const childNodeToBeDeleted = document.getElementById(emailId); //Unable to user querySelector
+    if(childNodeToBeDeleted != null){
+        parentNode.removeChild(childNodeToBeDeleted)
+    }
+}
+
+function deleteUser(emailId){
+    // console.log(emailId)
+    localStorage.removeItem(emailId);
+    removeUserFromScreen(emailId);
+}
+
+function editUserDetails(emailId, name, newItem){
+    document.querySelector('#email').value = emailId;
+    document.querySelector('#user').value = name;
+   // document.querySelector('#item').value = newItem;
+    deleteUser(emailId)
+ }
+
+
+
+// document.getElementById('get').addEventListener('click', getTodos);
+document.getElementById('user').addEventListener('click', saveToLocal);
+document.getElementById('email').addEventListener('click', saveToLocal);
+//document.getElementById('update').addEventListener('click', updateTodo);
+//document.getElementById('delete').addEventListener('click', removeTodo);
